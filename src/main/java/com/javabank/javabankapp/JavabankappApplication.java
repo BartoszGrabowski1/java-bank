09999688c50e496d1c81738bd6f90c56170a425f
BartoszGrabowski1 <98315350+BartoszGrabowski1@users.noreply.github.com>
2023-01-03 10:18:29 +0100

    Add files via upload

---
+++ src/main/java/com/javabank/javabankapp/JavabankappApplication.java
@@ -0,0 +1,13 @@
+package com.javabank.javabankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavabankappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavabankappApplication.class, args);
+	}
+
+}
